e64f6437ee663bfd1a195e089da36bbe6e207556
SimoneSpirito <simospiri01@gmail.com>
2021-12-10 19:17:12 +0100

    title + logo bonus

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Title/>
+    <logo/>
+  </div>
+</template>
+
+<script>
+import Title from './components/Title.vue'
+import logo from './components/logo.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Title,
+    logo
+  }
+}
+</script>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/logo.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <img src="https://www.boolean.careers/images/misc/logo.png" alt="boolean-logo">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'boolean'
+}
+</script>
+
+<style scoped>
+img {
+    width: 200px;
+    margin-top: 100px;
+}
+</style>
